9c72c169dba5a5dea0aba6468488cff72eb2c710
Julian WEBER <julian.weber@hotmail.fr>
2018-12-16 23:46:27 +0100

    Exported historic

--- src/main/java/sample/model/Historic.java
+++ src/main/java/sample/model/Historic.java
@@ -1,5 +1,7 @@
 package sample.model;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Historic {
@@ -24,11 +26,37 @@ public class Historic {
     public void addToSold(String sale) {
         System.out.println(sale);
         sold.add(sale);
+        exportHistoric();
     }
 
     public void addToPurchase(String buy) {
         System.out.println(buy);
         purchase.add(buy);
+        exportHistoric();
+    }
+
+    public void exportHistoric(){
+        String content = "";
+        content += "PURCHASE : \n";
+        for (int i = 0; i < purchase.size(); i++){
+            content += purchase.get(i);
+        }
+        content += "SOLD : \n";
+        for (int i = 0; i < sold.size(); i++){
+            content += sold.get(i);
+        }
+        try {
+            usingFileWriter(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void usingFileWriter(String fileContent) throws IOException
+    {
+        FileWriter fileWriter = new FileWriter("./historic.txt");
+        fileWriter.write(fileContent);
+        fileWriter.close();
     }
 
 
